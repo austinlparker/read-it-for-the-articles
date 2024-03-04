4fae1c81b5ce2988f68856b06ad4fe9b7566a7c9
Austin Parker <austin@ap2.io>
2024-03-03 18:04:56 -0600

    Initial commit

---
+++ server/data_filter.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+
+from atproto import models
+
+from server.logger import logger
+from server.database import db, Post
+
+
+def operations_callback(ops: defaultdict) -> None:
+    # Here we can filter, process, run ML classification, etc.
+    # After our feed alg we can save posts into our DB
+    # Also, we should process deleted posts to remove them from our DB and keep it in sync
+
+    # for example, let's create our custom feed that will contain all posts that contains alf related text
+
+    posts_to_create = []
+    for created_post in ops[models.ids.AppBskyFeedPost]['created']:
+        author = created_post['author']
+        record = created_post['record']
+
+        # print all texts just as demo that data stream works
+        post_with_images = isinstance(record.embed, models.AppBskyEmbedImages.Main)
+        inlined_text = record.text.replace('\n', ' ')
+        logger.info(
+            f'NEW POST '
+            f'[CREATED_AT={record.created_at}]'
+            f'[AUTHOR={author}]'
+            f'[WITH_IMAGE={post_with_images}]'
+            f': {inlined_text}'
+        )
+
+        # only alf-related posts
+        if 'alf' in record.text.lower():
+            reply_root = reply_parent = None
+            if record.reply:
+                reply_root = record.reply.root.uri
+                reply_parent = record.reply.parent.uri
+
+            post_dict = {
+                'uri': created_post['uri'],
+                'cid': created_post['cid'],
+                'reply_parent': reply_parent,
+                'reply_root': reply_root,
+            }
+            posts_to_create.append(post_dict)
+
+    posts_to_delete = ops[models.ids.AppBskyFeedPost]['deleted']
+    if posts_to_delete:
+        post_uris_to_delete = [post['uri'] for post in posts_to_delete]
+        Post.delete().where(Post.uri.in_(post_uris_to_delete))
+        logger.info(f'Deleted from feed: {len(post_uris_to_delete)}')
+
+    if posts_to_create:
+        with db.atomic():
+            for post_dict in posts_to_create:
+                Post.create(**post_dict)
+        logger.info(f'Added to feed: {len(posts_to_create)}')
---
+++ server/data_stream.py
@@ -0,0 +1,91 @@
+from collections import defaultdict
+
+from atproto import AtUri, CAR, firehose_models, FirehoseSubscribeReposClient, models, parse_subscribe_repos_message
+from atproto.exceptions import FirehoseError
+
+from server.database import SubscriptionState
+from server.logger import logger
+
+_INTERESTED_RECORDS = {
+    models.AppBskyFeedLike: models.ids.AppBskyFeedLike,
+    models.AppBskyFeedPost: models.ids.AppBskyFeedPost,
+    models.AppBskyGraphFollow: models.ids.AppBskyGraphFollow,
+}
+
+
+def _get_ops_by_type(commit: models.ComAtprotoSyncSubscribeRepos.Commit) -> defaultdict:
+    operation_by_type = defaultdict(lambda: {'created': [], 'deleted': []})
+
+    car = CAR.from_bytes(commit.blocks)
+    for op in commit.ops:
+        if op.action == 'update':
+            # we are not interested in updates
+            continue
+
+        uri = AtUri.from_str(f'at://{commit.repo}/{op.path}')
+
+        if op.action == 'create':
+            if not op.cid:
+                continue
+
+            create_info = {'uri': str(uri), 'cid': str(op.cid), 'author': commit.repo}
+
+            record_raw_data = car.blocks.get(op.cid)
+            if not record_raw_data:
+                continue
+
+            record = models.get_or_create(record_raw_data, strict=False)
+            for record_type, record_nsid in _INTERESTED_RECORDS.items():
+                if uri.collection == record_nsid and models.is_record_type(record, record_type):
+                    operation_by_type[record_nsid]['created'].append({'record': record, **create_info})
+                    break
+
+        if op.action == 'delete':
+            operation_by_type[uri.collection]['deleted'].append({'uri': str(uri)})
+
+    return operation_by_type
+
+
+def run(name, operations_callback, stream_stop_event=None):
+    while stream_stop_event is None or not stream_stop_event.is_set():
+        try:
+            _run(name, operations_callback, stream_stop_event)
+        except FirehoseError as e:
+            # here we can handle different errors to reconnect to firehose
+            raise e
+
+
+def _run(name, operations_callback, stream_stop_event=None):
+    state = SubscriptionState.get_or_none(SubscriptionState.service == name)
+
+    params = None
+    if state:
+        params = models.ComAtprotoSyncSubscribeRepos.Params(cursor=state.cursor)
+
+    client = FirehoseSubscribeReposClient(params)
+
+    if not state:
+        SubscriptionState.create(service=name, cursor=0)
+
+    def on_message_handler(message: firehose_models.MessageFrame) -> None:
+        # stop on next message if requested
+        if stream_stop_event and stream_stop_event.is_set():
+            client.stop()
+            return
+
+        commit = parse_subscribe_repos_message(message)
+        if not isinstance(commit, models.ComAtprotoSyncSubscribeRepos.Commit):
+            return
+
+        # update stored state every ~20 events
+        if commit.seq % 20 == 0:
+            logger.info(f'Updated cursor for {name} to {commit.seq}')
+            client.update_params(models.ComAtprotoSyncSubscribeRepos.Params(cursor=commit.seq))
+            SubscriptionState.update(cursor=commit.seq).where(SubscriptionState.service == name).execute()
+
+        if not commit.blocks:
+            return
+
+        operations_callback(_get_ops_by_type(commit))
+
+    client.start(on_message_handler)
